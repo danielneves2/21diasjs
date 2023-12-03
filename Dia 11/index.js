b96a05c8274c29c253c3e6fa0a8d2888db2075a2
Daniel Neves <danielneveslm@gmail.com>
2023-12-03 12:21:26 -0300

    atualizando o exercicio

--- Dia 11/index.js
+++ Dia 11/index.js
@@ -1,4 +1,42 @@
 
+//Tabela com nomes e notas + media da turma .
+
+let alunos = []
+let notas = []
+let continuar = true
+let contador = 0
+
+while(continuar){
+    let nome = prompt("Digite o nome do "+(contador +1)+" aluno: ")
+    let nota = Number(prompt("Digite a nota do aluno: "))
+    alunos[contador]=nome
+    notas[contador]= nota
+    contador ++
+    let resposta = prompt("Deseja continuar inserindo alunos ? (s/n)")
+    if (resposta === "n"){
+        continuar=false
+    }
+
+}
+
+//EXIBINDO AS INFO 
+
+console.log("NOTAS DOS ALUNOS !!!!")
+
+for(let i = 0 ; i < alunos.length; i++){
+    console.log(alunos[i]+" - "+ notas[i])
+
+}
+
+let somaDasNotas = 0
+
+for (let i = 0 ; i < notas.length; i++){
+    somaDasNotas += notas[i]
+}
+
+let media = somaDasNotas / alunos.length
+console.log ("A soma das notas dos alunos foi "+somaDasNotas    )
+console.log("A media geral foi :"+media)
 
 
 
@@ -31,27 +69,28 @@ for (let i = 0 ; i < modelos.length; i++){
     console.log(modelos[i]+" - "+anos[i]+" - "+valores[i])
 }
 
-// Ordenar carros do mais barato ao mais caro
-for (let i = 0; i < valores.length - 1; i++) {
-    for (let j = 0; j < valores.length - i - 1; j++) {
-        if (valores[j] > valores[j + 1]) {
-            // Trocar posição nos arrays modelos, anos e valores
-            let tempModelo = modelos[j];
-            modelos[j] = modelos[j + 1];
-            modelos[j + 1] = tempModelo;
+//ordenar por preco
 
-            let tempAno = anos[j];
-            anos[j] = anos[j + 1];
-            anos[j + 1] = tempAno;
+for(let i = 0 ; i < valores.length -1 ; i++){
+    for(let j = 0 ; j < valores.length -i -1; j++){
 
-            let tempValor = valores[j];
-            valores[j] = valores[j + 1];
-            valores[j + 1] = tempValor;
-        }
+        let modeloMaiorValor = modelos[j]
+        modelos[j] = modelos[j + 1]
+        modelos[j + 1] = modeloMaiorValor
+
+
+        let anoMaiorValor = anos[j]
+        anos[j] = anos[j+1]
+        anos[j + 1] = anoMaiorValor
+
+        let maiorValor = valores[j]
+        valores[j]= valores[j+1]
+        valores[j + 1 ] = maiorValor
     }
 }
 
-console.log("Carros ordenados do mais barato ao mais caro:");
-for (let i = 0; i < modelos.length; i++) {
-    console.log(modelos[i] + " - " + anos[i] + " - " + valores[i]);
+console.log("Carros ordenados pelo preco :")
+
+for (let i = 0 ; i < modelos.length; i++){
+    console.log(modelos[i]+" - "+ anos[i]+" - "+ valores[i])
 }
